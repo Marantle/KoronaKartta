a8555702953fda76adf276ca8ba00098cc5455f5
Marko Leppänen <anterolep@gmail.com>
2020-03-18 17:23:49 +0200

    husky test

--- utils/date.ts
+++ utils/date.ts
@@ -1,17 +1,19 @@
-import {CoronaData} from '../components/map/Map'
+import { CoronaData } from "../components/map/Map";
 
-export const extractDates = (coronaData: CoronaData)  => {
-  const formatDate = (d: Date): string =>
-    `${d.getFullYear()}-${String(1 + d.getMonth()).padStart(2, "0")}-${String(
-      d.getDate()
-    ).padStart(2, "0")}`;
+// formats as 2020-01-02
+const formatDate = (d: Date): string =>
+  `${d.getFullYear()}-${String(1 + d.getMonth()).padStart(2, "0")}-${String(
+    d.getDate()
+  ).padStart(2, "0")}`;
 
+export const extractDates = (coronaData: CoronaData) => {
   const confirmedDates = coronaData.rawInfectionData.confirmed
     .map(c => new Date(c.date))
     .map(formatDate);
   const recoveredDates = coronaData.rawInfectionData.recovered
     .map(c => new Date(c.date))
-    .map(formatDate)
-    const dates = [...new Set(...[confirmedDates, recoveredDates])].sort();
+    .map(formatDate);
+    // trim duplicates
+  const dates = [...new Set(...[confirmedDates, recoveredDates])].sort();
   return dates;
-}
+};
