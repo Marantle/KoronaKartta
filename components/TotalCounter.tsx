7399a4cbc3fb9b71caf258d813a7aa0cc076bfc5
Marko <mleppa@reaktor.fi>
2020-09-11 20:13:16 +0300

    feat: map styling update

--- components/TotalCounter.tsx
+++ components/TotalCounter.tsx
@@ -1,33 +1,8 @@
+import { Typography, useTheme } from "@material-ui/core";
 import CSS from "csstype";
-import { TotalCounts } from "./map/Map";
-import { isDarkMode } from "../utils/dark";
 import React from "react";
-import { Typography } from "@material-ui/core";
-
-let tableStyle: CSS.Properties = {
-  zIndex: 1000,
-  position: "absolute",
-  left: "1%",
-  top: "1%",
-  backgroundColor: "#fff",
-  border: "1px solid black",
-  fontSize: "12px",
-  padding: "5px",
-};
-
-if (isDarkMode) {
-  tableStyle = {
-    zIndex: 1000,
-    position: "absolute",
-    left: "1%",
-    top: "1%",
-    backgroundColor: "#000",
-    color: "#fff",
-    border: "1px solid white",
-    fontSize: "12px",
-    padding: "5px",
-  };
-}
+import { isDarkMode } from "../utils/dark";
+import { TotalCounts } from "./map/Map";
 
 const headerStyle: CSS.Properties = {
   borderBottom: "1px solid #dd",
@@ -47,13 +22,40 @@ const rowStyle: CSS.Properties = {
 };
 
 const TotalCounter = (props: TotalCounts) => {
+  const theme = useTheme();
+  let tableStyle: CSS.Properties = {
+    backgroundColor: theme.palette.primary.light,
+    border: "1px solid black",
+  };
+
+  if (isDarkMode) {
+    tableStyle = {
+      backgroundColor: theme.palette.primary.dark,
+      color: "#fff",
+      border: "1px solid white",
+    };
+  }
+  const finalTableStyle = {
+    ...tableStyle,
+    zIndex: 1000,
+    padding: "5px",
+  };
+  if (!props.title) {
+    finalTableStyle.position = "absolute";
+    finalTableStyle.left = "1%";
+    finalTableStyle.top = "1%";
+  } else {
+    finalTableStyle.border = undefined;
+    finalTableStyle.padding = "0";
+    finalTableStyle.margin = "-5px";
+  }
   return (
-    <div style={props.title ? {} : tableStyle}>
-      <table className="popuptable">
+    <div style={finalTableStyle}>
+      <table className="">
         <thead>
           <tr>
             <th colSpan={2} style={headerStyle}>
-              <Typography variant="h6" component="h1">
+              <Typography noWrap variant="h5" component="h2">
                 {props.title ?? "KOKO SUOMI"}
               </Typography>
             </th>
@@ -62,7 +64,7 @@ const TotalCounter = (props: TotalCounts) => {
         <tbody>
           <tr>
             <td style={rowTitleStyle}>
-              <Typography variant="subtitle1" component="h2">
+              <Typography variant="subtitle1" component="h2" noWrap>
                 Menehtyneitä
               </Typography>
             </td>
@@ -74,12 +76,21 @@ const TotalCounter = (props: TotalCounts) => {
           </tr>
           <tr style={{ fontWeight: "bold" }}>
             <td style={rowTitleStyle}>
-              <Typography noWrap variant="subtitle1" component="h2">
-                Tartuntoja kaikkiaan
+              <Typography
+                noWrap
+                variant="subtitle1"
+                component="h2"
+                style={{ fontWeight: "bold" }}
+              >
+                Tartuntoja
               </Typography>
             </td>
             <td style={rowStyle}>
-              <Typography noWrap variant="subtitle1" component="h2">
+              <Typography
+                variant="subtitle1"
+                component="h2"
+                style={{ fontWeight: "bold" }}
+              >
                 {props.allInfections}
               </Typography>
             </td>
